448e82d213fc670e3636ee859926d332da2159ed
A6ih <177154114+A6ih@users.noreply.github.com>
2025-04-02 16:59:47 +0300

    Add toString() method to LinkedList class

--- index.js
+++ index.js
@@ -1,80 +1,116 @@
 class Node {
-    constructor(value = null) {
-        this.value = value,
-        this.nextNode = null
-    }
+  constructor(value = null) {
+    (this.value = value), (this.nextNode = null);
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  append(value) {
+    if (this.head === null) {
+      const node = new Node(value);
+      this.head = node;
+      this.tail = node;
+      this.size++;
+    } else {
+      this.tail.nextNode = new Node(value);
+      this.tail = this.tail.nextNode;
+      this.size++;
+    }
+  }
+
+  prepend(value) {
+    if (this.head === null) {
+      const node = new Node(value);
+      this.head = node;
+      this.tail = node;
+      this.size++;
+    } else {
+      const node = new Node(value);
+      node.nextNode = this.head;
+      this.head = node;
+      this.size++;
+    }
+  }
+
+  at(index) {
+    let currentNode = this.head;
+    let count = 0;
+
+    while (currentNode) {
+      if (index === count) {
+        return currentNode;
+      }
+
+      count++;
+      currentNode = currentNode.nextNode;
+    }
+    return -1;
+  }
+
+  pop() {
+    if (this.head === null) {
+      return;
+    }
+    this.size--;
+    this.tail = this.at(this.size - 1);
+    this.tail.nextNode = null;
+  }
+
+  contains(value) {
+    let current = this.head;
+
+    while (current) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.nextNode;
     }
 
-    append(value) {
-        if(this.head === null) {
-            const node = new Node(value)
-            this.head = node
-            this.tail = node
-            this.size++
-        } else {
-            this.tail.nextNode = new Node(value)
-            this.tail = this.tail.nextNode
-            this.size++
-        }
+    return false;
+  }
+
+  find(value) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (current.value === value) {
+        return count;
+      }
+      current = current.nextNode;
+      count++;
     }
 
-    prepend(value) {
-        if(this.head === null) {
-            const node = new Node(value)
-            this.head = node
-            this.tail = node
-            this.size++
-        } else {
-            const node = new Node(value)
-            node.nextNode = this.head
-            this.head = node
-            this.size++
-        }
+    return null;
+  }
+
+  toString() {
+    let current = this.head;
+    let string = "";
+    let count = 0;
+
+    if (current === null) {
+      return (string = "null");
     }
 
-    at(index) {
-        let currentNode = this.head
-        let count = 0
-
-        while(currentNode) {
-            if (index === count) {
-                return currentNode
-            }
-
-            count++
-            currentNode = currentNode.nextNode
-        }
-        return -1;
+    while (current) {
+      if (count === 0) {
+        string += `( ${current.value} )`;
+      } else if (current.nextNode === null) {
+        return string += ` -> ( ${current.value} ) -> ${current.nextNode}`
+      } else {
+        string+= ` -> ( ${current.value} )`
+      }
+      count++
+      current = current.nextNode
     }
-    
-    pop() {
-        if(this.head === null) {
-            return
-        }
-        this.size--
-        this.tail = this.at(this.size - 1)
-        this.tail.nextNode = null
-    }
-
-    contains(value) {
-        let current = this.head
-        
-        while(current) {
-            if(current.value === value) {
-                return true
-            }
-            current = current.nextNode
-        }
-
-        return false
-    }
+  }
 }
 
 const list = new LinkedList();
@@ -83,6 +119,9 @@ list.append("dog");
 list.append("cat");
 list.append("hippo");
 list.append("girafee");
-list.prepend("Awof");
+list.prepend("elephant");
+list.append("lion");
+list.prepend("monkey");
+list.append("gorilla");
 
-console.log(list.contains("hippo"));
+console.log(list.toString());
